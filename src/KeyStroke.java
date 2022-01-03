25c9ccfd2a98bdce3384268d1c88c4599430f477
Wingemo <philip.wingemo@outlook.com>
2022-01-03 23:05:47 +0100

    Update KeyStroke.java

--- src/KeyStroke.java
+++ src/KeyStroke.java
@@ -1,1 +1,40 @@
+public static void search(){
+        robot.keyPress(KeyEvent.VK_WINDOWS);
+        robot.keyRelease(KeyEvent.VK_WINDOWS);
+    }
+
+    public static void enter(){
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+    }
+
+    public static void tab(){
+        robot.keyPress(KeyEvent.VK_TAB);
+        robot.keyRelease(KeyEvent.VK_TAB);
+    }
+
+    public static void capslock(){
+        robot.keyPress(KeyEvent.VK_CAPS_LOCK);
+        robot.keyRelease(KeyEvent.VK_CAPS_LOCK);
+    }
+
+    public static void f10(){
+        robot.keyPress(KeyEvent.VK_F10);
+        robot.keyRelease(KeyEvent.VK_F10);
+    }
+
+    public static void down(){
+        robot.keyPress(KeyEvent.VK_DOWN);
+        robot.keyRelease(KeyEvent.VK_DOWN);
+    }
+
+    public static void up(){
+        robot.keyPress(KeyEvent.VK_UP);
+        robot.keyRelease(KeyEvent.VK_UP);
+    }
+
+    public static void control(){
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+    }
 
